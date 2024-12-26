59c5d289ffeced7bfe270c90ec55e500a12a2fd5
zarila00 <shyreenkhoza8@gmail.com>
2024-12-25 18:04:57 -0800

    bottom nav bar

--- lib/Pages/BottomNavbar.dart
+++ lib/Pages/BottomNavbar.dart
@@ -25,7 +25,67 @@ class _BottomnavbarState extends State<Bottomnavbar> {
       body: AnimatedSwitcher(
         duration: const Duration(milliseconds: 300),
         child: _pages[_selectedIndex],
-        ),
+      ),
+      bottomNavigationBar: _buildBottomNavigationBar(),
+    );
+  }
+
+  Widget _buildBottomNavigationBar() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(30),
+        boxShadow: [
+          BoxShadow(
+              color: Colors.black12.withOpacity(0.2),
+              spreadRadius: 3,
+              blurRadius: 10)
+        ],
+      ),
+      child: BottomNavigationBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        items: [
+          _buildBottomNavItem(Icons.home, 'Home', 0),
+          _buildBottomNavItem(Icons.store, 'MarketPlace', 1),
+          _buildBottomNavItem(Icons.contact_mail, 'Expert', 2),
+          _buildBottomNavItem(Icons.book, 'ResourceHub', 3),
+          _buildBottomNavItem(Icons.person, 'Profile', 4),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.grey,
+        unselectedItemColor: Colors.orange,
+        selectedFontSize: 13,
+        unselectedFontSize: 12,
+        showSelectedLabels: true,
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed,
+      ),
+    );
+  }
+
+  BottomNavigationBarItem _buildBottomNavItem(
+      IconData icon, String label, int index) {
+    final bool isSelected = _selectedIndex == index;
+
+    return BottomNavigationBarItem(
+        icon: AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeInCubic,
+      padding: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        color: isSelected ? Colors.grey.withOpacity(0.2) : Colors.transparent,
+        shape: BoxShape.circle
+      ),
+      child: Icon(
+        icon,
+        color: isSelected ? Colors.orange: Colors.grey[400],
+        size:  isSelected ? 28 : 24,
+      ),
+    ),
+    label: label
+    
     );
   }
 }
