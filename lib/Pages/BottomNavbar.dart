5f702e038890afb777d884d645e004f95f370bdd
zarila00 <shyreenkhoza8@gmail.com>
2024-12-25 17:25:50 -0800

    bottom nav bar

---
+++ lib/Pages/BottomNavbar.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Bottomnavbar extends StatefulWidget {
+  const Bottomnavbar({super.key});
+
+  @override
+  State<Bottomnavbar> createState() => _BottomnavbarState();
+}
+
+class _BottomnavbarState extends State<Bottomnavbar> {
+
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
