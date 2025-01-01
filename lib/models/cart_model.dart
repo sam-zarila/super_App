8cc582d8db9a1241167dab0481ff4a638aabd90d
sam-zarila <zarilatroxx99@gmail.com>
2025-01-01 14:32:46 -0800

    cart functionality

--- lib/models/cart_model.dart
+++ lib/models/cart_model.dart
@@ -4,4 +4,28 @@ class CartModel {
   final int quantity;
   final String image;
   final String name;
+  final String price;
+  final String description;
+
+  CartModel({
+    required this.id,
+    required this.item,
+    required this.quantity,
+    required this.image,
+    required this.name,
+    required this.price,
+    required this.description,
+  });
+
+  factory CartModel.fromJson(Map<String, dynamic> json){
+    return CartModel(
+      id: json['id'],
+      item: json['item'],
+      quantity: json['quantity'],
+      image: json['image'],
+      name: json['name'],
+      price: json['price'],
+      description: json['description'],
+    );
+  }
 }
---
+++ lib/services/cart_services.dart
@@ -0,0 +1,21 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import '../models/cart_model.dart';
+
+class CartService {
+  final String baseUrl;
+
+  CartService(this.baseUrl);
+
+  Future<void> addToCart(CartModel cartItem) async {
+    final url = '$baseUrl/cart';
+
+    final response = await http.post(Uri.parse(url),
+     headers: {
+      'Content-Type': 'application/json',
+     },
+     body: json.encode(cartItem.toJSON()),
+    
+    );
+  }
+}
