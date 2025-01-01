878f550f3988dd439ea57c11638c916a177fa509
sam-zarila <zarilatroxx99@gmail.com>
2025-01-01 15:21:27 -0800

    cart functionality

---
+++ lib/Pages/cartpage.dart
@@ -0,0 +1,26 @@
+import '../models/cart_model.dart';
+import '../services/cart_services.dart';
+import 'package:flutter/material.dart';
+
+class Cartpage extends StatefulWidget {
+  final CartService cartService;
+
+    const Cartpage({
+    required this.cartService, Key? key}) : super(key: key);
+
+  @override
+  State<Cartpage> createState() => _CartpageState();
+}
+
+class _CartpageState extends State<Cartpage> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text('My Cart'),
+      ),
+      body: FutureBuilder<List<CartModel>>(
+        future: future, builder: builder),
+    );
+  }
+}
--- lib/services/cart_services.dart
+++ lib/services/cart_services.dart
@@ -10,18 +10,30 @@ class CartService {
   Future<void> addToCart(CartModel cartItem) async {
     final url = '$baseUrl/cart';
 
-    final response = await http.post(Uri.parse(url),
-     headers: {
-      'Content-Type': 'application/json',
-     },
-     body: json.encode(cartItem.toJSON()),
-    
+    final response = await http.post(
+      Uri.parse(url),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: json.encode(cartItem.toJSON()),
     );
 
     if (response.statusCode == 200 || response.statusCode == 201) {
-      
+      print('Item added successfully: ${response.body}');
     } else {
-      
+      throw Exception('Failed to add item to cart: ${response.statusCode} - ${response.body}');
+    }
+  }
+  Future<List<CartModel>> fetchCartItems() async {
+    final url = '$baseUrl/cart';
+
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      List<dynamic> jsonResponse = json.decode(response.body);
+      return jsonResponse.map((item) => CartModel.fromJSON(item)).toList();
+    } else {
+      throw Exception('Failed to fetch cart items: ${response.body}');
     }
   }
 }
