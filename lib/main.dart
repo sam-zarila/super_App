31d8c16da08478e8fb51af369b8cea0df40ce9f5
sam-zarila <zarilatroxx99@gmail.com>
2025-01-03 12:53:12 -0800

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'Pages/BottomNavbar.dart';
+import 'firebase_options.dart';  // This file will be generated after Firebase setup
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
+  // Initialize Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);  // Use the generated file
+  
+  runApp(const MyApp());
+}
 
-  runApp(const MaterialApp(
-    home: Bottomnavbar(),
-  ));
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Bottomnavbar(),
+    );
+  }
 }
