39c5d03e6151b07e8c309a866a407641f9b21b9e
sam-zarila <zarilatroxx99@gmail.com>
2025-01-10 15:15:22 -0800

    cartpage

---
+++ lib/models/aunthentication_model.dart
@@ -0,0 +1,22 @@
+class UserModel {
+  final String id;
+  final String name;
+  final String email;
+  final String password;
+
+  UserModel({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.password,
+  });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      id: json['id'],
+      name: json['name'],
+      email: json['email'],
+      password: json['password'],
+    );
+  }
+}
