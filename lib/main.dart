74ccfff39f1c7fa1c2857d97a58a767d5938ae61
sam-zarila <zarilatroxx99@gmail.com>
2025-01-11 17:27:11 -0800

    homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:super_app/Pages/BottomNavbar.dart';
 import 'package:super_app/Pages/cartpage.dart';
 import 'package:super_app/services/cart_services.dart';
 import 'package:super_app/signup/Login.dart';
@@ -14,7 +15,7 @@ void main() {
      routes: {
         '/signup': (context) => SignupPage(),
         '/login': (context) => LoginPage(),
-         
+        '/bottomnavbar': (context) => Bottomnavbar()    
      
       },
   ));
