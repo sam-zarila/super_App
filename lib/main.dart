1abae4c9e5f931aa81986c02d8d8dd868bf32901
sam-zarila <zarilatroxx99@gmail.com>
2025-01-11 18:03:46 -0800

    homepage

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,35 @@ import 'package:super_app/Pages/cartpage.dart';
 import 'package:super_app/services/cart_services.dart';
 import 'package:super_app/signup/Login.dart';
 import 'package:super_app/signup/Signup.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Ensure that widget binding is initialized before accessing SharedPreferences
+  
+  final prefs = await SharedPreferences.getInstance();
+  final token = prefs.getString('token'); // Check if token exists
 
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: 'University Business App',
-    initialRoute: '/login',
-    // home: Bottomnavbar(), 
-     routes: {
+  runApp(MyApp(token: token)); // Pass token to the app
+}
+
+class MyApp extends StatelessWidget {
+  final String? token;
+
+  const MyApp({Key? key, required this.token}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'University Business App',
+     // initialRoute: token != null ? '/home' : '/login', // Check if token exists to determine initial route
+     home: Bottomnavbar(),
+      routes: {
         '/signup': (context) => SignupPage(),
         '/login': (context) => LoginPage(),
-         '/cartpage': (context) => Cartpage(cartService: CartService('http://127.0.0.1:3000/cart'),), 
-     
+        // '/home': (context) => Bottomnavbar(), 
+        '/cartpage': (context) => Cartpage(cartService: CartService('http://127.0.0.1:3000/cart')),
       },
-  ));
+    );
+  }
 }
